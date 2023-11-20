86c1f62f3ed13e50dedf851335bc49b3e519966a
yusifhuseini <yusifhuseini@gmail.com>
2023-11-20 03:47:17 -0800

    sorting_algorithms

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,85 +1,110 @@
 #include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- *merge_sort - Sorts an array of integers in ascending order using Merge sort
- *
- *@array: The array to be sorted
- *@size: Number of elements in the array
+ *merge - Merges two halves of an array.
+ *@start: Starting index of the first half.
+ *@middle: Ending index of the first half and starting index of the second half
+ *@end: Ending index of the second half.
+ *@dest: The dest array where the merged elements will be stored.
+ *@source: The source array to be merged.
  */
-void merge_sort(int *array, size_t size)
-{
-	/*Declare variables at the beginning of the block */
-	size_t mid, left_size, right_size;
-	int *left, *right;
-
-	if (size < 2)
-		return;
-
-	mid = size / 2;
-	left_size = mid;
-	right_size = size - mid;
-
-	left = array;
-	right = array + mid;
-
-	/*Recursive calls to sort the left and right subarrays */
-	merge_sort(left, left_size);
-	merge_sort(right, right_size);
-
-	/*Merge the sorted left and right subarrays */
-	merge(array, size, left, left_size, right, right_size);
-}
-
-/**
- *merge - Merges two sorted subarrays into a single sorted array
- *
- *@array: The array to be merged
- *@size: Number of elements in the array
- *@left: The left subarray
- *@left_size: Number of elements in the left subarray
- *@right: The right subarray
- *@right_size: Number of elements in the right subarray
- */
-void merge(int *array, size_t size, int *left,
-				size_t left_size, int *right, size_t right_size)
+void merge(size_t start, size_t middle, size_t end, int *dest, int *source)
 {
 	size_t i, j, k;
-	int *merged = malloc(size * sizeof(int));
 
-	printf("Merging...\n[left]: ");
-	print_array(left, left_size);
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(source + start, middle - start);
 	printf("[right]: ");
-	print_array(right, right_size);
-	i = j = k = 0;
-	while (i < left_size && j < right_size)
+	print_array(source + middle, end - middle);
+
+	i = start;
+	j = middle;
+	k = start;
+
+	/*Merge the two halves */
+	while (k < end)
 	{
-		if (left[i] <= right[j])
+		if (i < middle && (j >= end || source[i] <= source[j]))
 		{
-			merged[k] = left[i];
+			dest[k] = source[i];
 			i++;
 		}
 		else
 		{
-			merged[k] = right[j];
+			dest[k] = source[j];
 			j++;
 		}
+
 		k++;
 	}
-	while (i < left_size)
-	{
-		merged[k] = left[i];
-		i++;
-		k++;
-	}
-	while (j < right_size)
-	{
-		merged[k] = right[j];
-		j++;
-		k++;
-	}
-	for (i = 0; i < size; i++)
-		array[i] = merged[i];
+
 	printf("[Done]: ");
-	print_array(merged, size);
-	free(merged);
+	print_array(dest + start, end - start);
+}
+
+/**
+ *split_and_merge - Recursively splits and merges the array.
+ *@start: Starting index of the subarray.
+ *@end: Ending index of the subarray.
+ *@array: The array to be sorted.
+ *@copy: Temporary array for merging.
+ */
+void split_and_merge(size_t start, size_t end, int *array, int *copy)
+{
+	size_t middle;
+
+	/*Base case: If the subarray has 1 or 0 elements, it is already sorted */
+	if (end - start < 2)
+		return;
+
+	middle = (start + end) / 2;
+
+	/*Recursively split and merge the left and right halves */
+	split_and_merge(start, middle, array, copy);
+	split_and_merge(middle, end, array, copy);
+
+	/*Merge the two sorted halves */
+	merge(start, middle, end, array, copy);
+
+	for (middle = start; middle < end; middle++)
+		copy[middle] = array[middle];
+}
+
+/**
+ *merge_sort - Sorts an array using the merge sort algorithm.
+ *@array: The array to be sorted.
+ *@size: The size of the array.
+ */
+void merge_sort(int *array, size_t size)
+{
+	size_t i;
+	int *copy;
+
+	/*Check if array is NULL or has less than 2 elements */
+	if (array == NULL || size < 2)
+		return;
+
+	/*Allocate memory for a temporary array */
+	copy = malloc(sizeof(int) * size);
+
+	/*Check if memory allocation was successful */
+	if (copy == NULL)
+		return;
+
+	i = 0;
+
+	/*Copy elements from array to the temporary array */
+	do {
+		copy[i] = array[i];
+		i++;
+	} while (i < size);
+
+	/*Perform the merge sort algorithm */
+	split_and_merge(0, size, array, copy);
+
+	/*Free the allocated memory */
+	free(copy);
 }
