b4ed9089c639a31f20387928c8101564fff83cc8
yusifhuseini <yusifhuseini@gmail.com>
2023-11-20 11:36:43 -0800

    /sorting_algorithms

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,116 +1,122 @@
 #include "deck.h"
+#include <stddef.h>
 
 /**
- *sort_deck - Sorts a deck of cards in ascending order based on card values.
- *@deck: Pointer to the head of the deck
+ *sort_deck - Sorts a deck of cards in ascending order based on their values.
+ *
+ *@deck: A pointer to the head of the deck.
  */
 void sort_deck(deck_node_t **deck)
 {
-	size_t deck_length;
-	deck_node_t *current, *prev_two, *prev_one, *current_next;
-	deck_node_t *curr;
+	size_t len;
+	deck_node_t *curr, *one, *two, *three, *four;
 
-	deck_length = list_length_deck(*deck);
-
-	if (!deck || !*deck || deck_length < 2)
+	/*Check if the deck is empty or has only one card */
+	if (!deck || !*deck)
 		return;
 
-	for (curr = *deck; curr != NULL; curr = curr->next)
+	len = list_len_deck(*deck);
+
+	if (len < 2)
+		return;
+
+	/*Bubble sort algorithm to sort the deck */
+	for (curr = *deck; curr; curr = curr->next)
 	{
 		if (curr->prev && card_value(curr) < card_value(curr->prev))
 		{
-			prev_two = curr->prev->prev;
-			prev_one = curr->prev;
-			current = curr;
-			current_next = curr->next;
+		    /*Swapping cards */
+			one = curr->prev->prev;
+			two = curr->prev;
+			three = curr;
+			four = curr->next;
 
-			prev_one->next = current_next;
-			if (current_next)
-				current_next->prev = prev_one;
-			current->next = prev_one;
-			current->prev = prev_two;
-			if (prev_two)
-				prev_two->next = current;
+			two->next = four;
+			if (four)
+				four->prev = two;
+			three->next = two;
+			three->prev = one;
+			if (one)
+				one->next = three;
 			else
-				*deck = current;
-			prev_one->prev = current;
-			curr = *deck;
+				*deck = three;
+			two->prev = three;
+			curr = *deck; /*Resetting the current pointer */
 		}
 	}
 }
 
 /**
- *card_value - Calculates the value of a card in the deck.
- *@node: Pointer to the node containing the card
+ *card_value - Returns the numeric value of a card.
  *
- *Return: The calculated value of the card.
+ *@node: A pointer to the deck_node_t structure representing the card.
+ *
+ *Return: The numeric value of the card.
  */
 int card_value(deck_node_t *node)
 {
-	char *card_values[13] = { "Ace", "2", "3", "4", "5", "6",
+	char *val[13] = { "Ace", "2", "3", "4", "5", "6",
 		"7", "8", "9", "10", "Jack", "Queen", "King" };
 
-	char *card_suits[4] = { "SPADE", "HEART", "CLUB", "DIAMOND" };
+	char *kinds[4] = { "SPADE", "HEART", "CLUB", "DIAMOND" };
 
-	int i, value = 0;
+	int i = 1, kind_val = 0;
 
-	i = 1;
-	while (i <= 13 && _strcmp(node->card->value, card_values[i - 1]) != 0)
+	/*Determine the rank value */
+	while (i <= 13 && _strcmp(node->card->value, val[i - 1]) != 0)
 	{
-		value = i;
+		kind_val = i;
 		i++;
 	}
 
+	/*Determine the suit value */
 	i = 1;
-	while (i <= 4 && _strcmp(card_suits[node->card->kind],
-								card_suits[i - 1]) != 0)
+	while (i <= 4 && _strcmp(kinds[node->card->kind], kinds[i - 1]) != 0)
 	{
-		value = value + (13 * i);
+		kind_val = kind_val + (13 * i);
 		i++;
 	}
 
-	return (value);
+	return (kind_val);
 }
 
 /**
- *_strcmp - Compares two strings.
- *@str1: First string
- *@str2: Second string
+ *_strcmp - Compares two strings lexicographically.
  *
- *Return: 0 if strings are equal, positive if str1 is greater,
- *negative if str2 is greater
+ *@str1: The first string.
+ *@str2: The second string.
+ *
+ *Return: An integer greater than, equal to, or less than 0, according to
+ *        the first string is greater than, equal to, or less than the second.
  */
 int _strcmp(const char *str1, const char *str2)
 {
-	while (*str1 == *str2)
-	{
+	do {
 		if (*str1 == '\0')
 		{
 			return (0);
 		}
-
-		str1++;
-		str2++;
-	}
+	} while (*str1++ == *str2++);
 
 	return (*str1 - *str2);
 }
 
 /**
- *list_length_deck - Calculates the length of a deck.
- *@list: Pointer to the head of the deck
+ *list_len_deck - Returns the length of a linked list of a deck of cards.
  *
- *Return: The length of the deck.
+ *@list: A pointer to the head of the deck.
+ *
+ *Return: The number of cards in the deck.
  */
-size_t list_length_deck(deck_node_t *list)
+size_t list_len_deck(deck_node_t *list)
 {
-	size_t length = 0;
+	size_t len = 0;
 
-	while (list)
-	{
-		length++;
+	/*Count the number of nodes in the linked list */
+	do {
+		len++;
 		list = list->next;
-	}
+	} while (list);
 
-	return (length);
+	return (len);
 }
