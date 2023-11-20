571c73579767420a0b87a6c4f4e4dda244799e9e
yusifhuseini <yusifhuseini@gmail.com>
2023-11-20 05:51:49 -0800

    sorting_algorithms

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,70 +1,93 @@
 #include "sort.h"
+#include <stdlib.h>
 
 /**
- *radix_sort - Sorts an array of integers using the Radix sort algorithm
- *@array: The array to be sorted
- *@size: Number of elements in the array
+ * power_of_10 - Compute the power of 10 for a given exponent.
+ * @power: The exponent for the power of 10.
+ *
+ * Return: The result of 10 raised to the power
+ */
+unsigned int power_of_10(unsigned int power)
+{
+	unsigned int i, result;
+
+	result = 1;
+	i = 0;
+	while (i < power)
+	{
+		result *= 10;
+		i++;
+	}
+
+	return (result);
+}
+
+/**
+ * count_sort - sorts Perform counting sort based on a specific digit place.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ * @digit: The digit place to consider for sorting.
+ *
+ * Return: 1 if sorting is performed, 0 otherwise.
+ */
+unsigned int count_sort(int *array, size_t size, unsigned int digit)
+{
+	int count[10] = { 0 };
+
+	int i, *copy = NULL;
+	size_t j, temp, total = 0;
+	unsigned int divisor1, divisor2, sort = 0;
+
+	divisor2 = power_of_10(digit - 1);
+	divisor1 = divisor2 * 10;
+	copy = malloc(sizeof(int) * size);
+	if (copy == NULL)
+		exit(1);
+	/*Copy the array to 'copy' array */
+	for (j = 0; j < size; j++)
+	{
+		copy[j] = array[j];
+		if (array[j] / divisor1 != 0)
+			sort = 1;
+	}
+
+	/*Initialize count array to 0 using a for loop */
+	for (i = 0; i < 10; i++)
+		count[i] = 0;
+	/*Count occurrences of each digit using a for loop */
+	for (j = 0; j < size; j++)
+		count[(array[j] % divisor1) / divisor2] += 1;
+	/*Update count array with cumulative counts using a for loop */
+	for (i = 0; i < 10; i++)
+	{
+		temp = count[i];
+		count[i] = total;
+		total += temp;
+	}
+	/*Update the original array using count array &'copy' array using for loop */
+	for (j = 0; j < size; j++)
+	{
+		array[count[(copy[j] % divisor1) / divisor2]] = copy[j];
+		count[(copy[j] % divisor1) / divisor2] += 1;
+	}
+	free(copy); /*Free dynamically allocated memory */
+	return (sort);
+}
+
+/**
+ * radix_sort - Perform radix sort on an array.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
  */
 void radix_sort(int *array, size_t size)
 {
-	int max = getMax(array, size);
-	int exp;
+	unsigned int i = 1, sort = 1;
 
-	for (exp = 1; max / exp > 0; exp *= 10)
-	{
-		countingSort(array, size, exp);
+	if (array == NULL || size < 2)
+		return;
+	do {
+		sort = count_sort(array, size, i);
 		print_array(array, size);
-	}
-}
-
-/**
- *getMax - Gets the maximum value in an array
- *@array: The array
- *@size: Number of elements in the array
- *Return: The maximum value
- */
-int getMax(int *array, size_t size)
-{
-	int max = array[0];
-	size_t i;
-
-	for (i = 1; i < size; i++)
-	{
-		if (array[i] > max)
-			max = array[i];
-	}
-
-	return (max);
-}
-
-/**
- *countingSort - Performs counting sort on the array based
- *on the digit represented by exp
- *@array: The array to be sorted
- *@size: Number of elements in the array
- *@exp: The current digit position (1, 10, 100, ...)
- */
-void countingSort(int *array, size_t size, int exp)
-{
-	int *output = malloc(sizeof(int) * size);
-	int count[10] = { 0 };
-
-	size_t i;
-
-	for (i = 0; i < size; i++)
-		count[(array[i] / exp) % 10]++;
-
-	for (i = 1; i < 10; i++)
-		count[i] += count[i - 1];
-
-	for (i = size - 1; i != SIZE_MAX; i--)
-	{
-		output[count[(array[i] / exp) % 10] - 1] = array[i];
-		count[(array[i] / exp) % 10]--;
-	}
-
-	for (i = 0; i < size; i++)
-		array[i] = output[i];
-
-	free(output);
+		i++;
+	} while (sort == 1);
 }
