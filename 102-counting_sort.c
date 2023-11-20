4e7346f6fd7162dbc0b7fe100f257a22a36a3d7b
yusifhuseini <yusifhuseini@gmail.com>
2023-11-19 18:03:50 -0800

    sorting_algorithms

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,50 +1,53 @@
 #include "sort.h"
+#include "stdlib.h"
 
 /**
- *counting_sort - sorts an array with the Counting sort algorithm
- *@array: array to sort
- *@size: size of the array
+ *counting_sort - Sorts an array of integers in ascending order
+ *using the counting sort algorithm.
+ *
+ *@array: The array to be sorted.
+ *@size: The size of the array.
  */
 void counting_sort(int *array, size_t size)
 {
-	size_t i;
-	int max_value = array[0];
-	int current_value;
-	int *count_array, *output_array;
+	int i, max_value;
+	int *count_array = NULL, *temp_array = NULL;
+	size_t j, original_value, cumulative_count = 0;
 
-	if (size < 2 || !array)
+	if (array == NULL || size < 2)
 		return;
-
-	for (i = 1; i < size; i++)
+	temp_array = malloc(sizeof(int) * size);
+	if (temp_array == NULL)
+		return;
+	for (j = 0, max_value = 0; j < size; j++)
 	{
-		if (array[i] > max_value)
-			max_value = array[i];
+		temp_array[j] = array[j];
+		if (array[j] > max_value)
+			max_value = array[j];
 	}
 	count_array = malloc(sizeof(int) * (max_value + 1));
-	output_array = malloc(sizeof(int) * size);
-
-	for (i = 0; i <= (size_t) max_value; i++)
+	if (count_array == NULL)
+	{
+		free(temp_array);
+		return;
+	}
+	for (i = 0; i <= max_value; i++)
 		count_array[i] = 0;
-
-	for (i = 0; i < size; i++)
+	for (j = 0; j < size; j++)
+		count_array[array[j]] += 1;
+	for (i = 0; i <= max_value; i++)
 	{
-		current_value = array[i];
-		count_array[current_value]++;
+		original_value = count_array[i];
+		count_array[i] = cumulative_count;
+		cumulative_count += original_value;
 	}
-
-	for (i = 1; i <= (size_t) max_value; i++)
-		count_array[i] += count_array[i - 1];
-
+	for (j = 0; j < size; j++)
+	{
+		array[count_array[temp_array[j]]] = temp_array[j];
+		count_array[temp_array[j]] += 1;
+	}
 	print_array(count_array, max_value + 1);
-
-	for (i = size - 1; i < size; i--)
-	{
-		output_array[count_array[array[i]] - 1] = array[i];
-		count_array[array[i]]--;
-	}
-
-	for (i = 0; i < size; i++)
-		array[i] = output_array[i];
-	free(output_array);
+	/*Free allocated memory */
 	free(count_array);
+	free(temp_array);
 }
