fb41b222dedf31db2d3b0071a79bd1a59806be87
yusifhuseini <yusifhuseini@gmail.com>
2023-11-20 08:01:15 -0800

    sorting_algorithms

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- *bitonic_compare - Perform bitonic comparison and swap elements if needed
- *@up: 1 for ascending order, 0 for descending order
- *@array: Array to be compared and swapped
- *@size: Size of the array
+ * bitonic_compare - Perform bitonic comparison and swap elements if needed
+ * @up: 1 for ascending order, 0 for descending order
+ * @array: Array to be compared and swapped
+ * @size: Size of the array
  */
 void bitonic_compare(char up, int *array, size_t size)
 {
@@ -25,10 +25,10 @@ void bitonic_compare(char up, int *array, size_t size)
 }
 
 /**
- *bitonic_merge - Recursively apply bitonic merge to the array
- *@up: 1 for ascending order, 0 for descending order
- *@array: Array to be merged
- *@size: Size of the array
+ * bitonic_merge - Recursively apply bitonic merge to the array
+ * @up: 1 for ascending order, 0 for descending order
+ * @array: Array to be merged
+ * @size: Size of the array
  */
 void bitonic_merge(char up, int *array, size_t size)
 {
@@ -40,29 +40,30 @@ void bitonic_merge(char up, int *array, size_t size)
 }
 
 /**
- *bit_sort - Perform bitonic sort on the array
- *@up: 1 for ascending order, 0 for descending order
- *@array: Array to be sorted
- *@size: Size of the array
- *@t: Parameter for printing information about the merging process
+ * bit_sort - Perform bitonic sort on the array
+ * @up: 1 for ascending order, 0 for descending order
+ * @array: Array to be sorted
+ * @size: Size of the array
+ * @t: Parameter for printing information about the merging process
  */
 void bit_sort(char up, int *array, size_t size, size_t t)
 {
 	if (size < 2)
 		return;
-	printf("Merging[%lu/%lu] (%s):\n", size, t, (up == 1) ? "UP" : "DOWN");
+	printf("Merging [%lu/%lu] (%s):\n", size, t, (up == 1) ? "UP" : "DOWN");
 	print_array(array, size);
 	bit_sort(1, array, size / 2, t);
 	bit_sort(0, array + (size / 2), size / 2, t);
 	bitonic_merge(up, array, size);
-	printf("Result[%lu/%lu] (%s):\n", size, t, (up == 1) ? "UP" : "DOWN");
+	printf("Result [%lu/%lu] (%s):\n", size, t, (up == 1) ? "UP" : "DOWN");
 	print_array(array, size);
+
 }
 
 /**
- *bitonic_sort - Start the bitonic sort process
- *@array: Array to be sorted
- *@size: Size of the array
+ * bitonic_sort - Start the bitonic sort process
+ * @array: Array to be sorted
+ * @size: Size of the array
  */
 void bitonic_sort(int *array, size_t size)
 {
